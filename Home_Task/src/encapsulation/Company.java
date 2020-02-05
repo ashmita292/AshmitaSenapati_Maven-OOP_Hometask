a459b487c5e1caa45d2f81ba016b87636d93974d
AshmitaSenapati <ashmita.senapati@gmail.com>
2020-02-05 22:58:27 +0530

    program added

---
+++ Home_Task/src/encapsulation/Company.java
@@ -0,0 +1,10 @@
+package encapsulation;
+
+public class Company {
+	public static void main(String[] args)
+	   {
+		   Employee e=new Employee();
+		   e.setEmpId(101);
+		   System.out.println(e.getEmpId());
+	   }
+}
---
+++ Home_Task/src/encapsulation/Employee.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class Employee {
+	 private int emp_id;
+	  public void setEmpId(int emp_id1)
+	  {
+		  emp_id= emp_id1;
+	  }
+	  public int getEmpId()
+	  {
+		  return emp_id;
+	  }
+}
+
