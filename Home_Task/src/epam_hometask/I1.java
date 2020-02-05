41d834b314dd4005141c70a0b89c2931e6cf31eb
AshmitaSenapati <ashmita.senapati@gmail.com>
2020-02-05 22:49:47 +0530

    program added

---
+++ Home_Task/src/epam_hometask/I1.java
@@ -0,0 +1,6 @@
+// program on classes, interface
+package epam_hometask;
+
+public interface I1 {
+	void show();
+}
---
+++ Home_Task/src/epam_hometask/Test.java
@@ -0,0 +1,15 @@
+package epam_hometask;
+
+
+
+public class Test  implements I1  {
+	public void show()
+	   {
+		   System.out.println("1");
+	   }
+	   public static void main(String[] args)
+	   {
+		   Test t=new Test();
+		   t.show();
+	   }
+}
